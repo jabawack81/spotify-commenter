426407b7c2f84aaf96269825e1664fe67c34d19c
Fabbri Paolo <jabawack81@gmail.com>
2018-08-18 20:15:11 +0100

    add flash messages to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@
 
 # Application Helper module
 module ApplicationHelper
+  def flash_class(key)
+    case key
+    when "notice"
+      "alert-success"
+    else
+      "alert-#{key}"
+      # "alert-warning"
+      # "alert-info"
+      # "alert-warning"
+      # "alert-danger"
+    end
+  end
 end
