04f9d651775e40b49e5ba472bb664e26eb1cb427
Fabbri Paolo <jabawack81@gmail.com>
2018-08-18 17:15:49 +0100

    add avatar to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
 
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["display_name"] || "" if auth["info"]
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.display_name || "" if auth["info"]
+      user.avatar = auth.info.images.first.url
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_12_132637) do
+ActiveRecord::Schema.define(version: 2018_08_12_204533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2018_08_12_132637) do
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   add_foreign_key "comments", "users"
