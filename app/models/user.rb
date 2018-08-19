97ed0d7c44a2359d1028119e4be18d874bfd4f90
Fabbri Paolo <jabawack81@gmail.com>
2018-08-19 16:55:16 +0100

    add email to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   has_many :playlists
+  validates_uniqueness_of :email
+  validates_uniqueness_of :uid
 
   def self.create_with_omniauth(auth)
     create! do |user|
@@ -9,6 +11,7 @@ class User < ApplicationRecord
       user.uid = auth.uid
       user.name = auth.info.display_name || "" if auth["info"]
       user.avatar = auth.info.images.first.url
+        user.email = auth.info.email
     end
   end
 
