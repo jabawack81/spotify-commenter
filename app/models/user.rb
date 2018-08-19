853600be7e23b645c4f98271bf658edad46d1eee
Fabbri Paolo <jabawack81@gmail.com>
2018-08-19 16:55:55 +0100

    add dependent destroy to user playlist commection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :playlists
+  has_many :playlists, dependent: :destroy
   validates_uniqueness_of :email
   validates_uniqueness_of :uid
 
